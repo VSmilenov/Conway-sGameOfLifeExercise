267a132237c99b85f35a756ffd34085a4c16c592
Vasil Dimitrov <vasil.smilenov@gmail.com>
2022-04-13 14:37:36 +0300

    Initial Commit

---
+++ Conway'sGameOfLifeExercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Conway'sGameOfLifeExercise
+//
+//  Created by Vasil Smilenov on 4/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
